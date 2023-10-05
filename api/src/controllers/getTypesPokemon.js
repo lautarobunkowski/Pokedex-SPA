76f583c9fbd4b5ef01e30df05065ed4076b303e4
Lautaro Bunkowski <lautibunko@gmail.com>
2023-10-05 15:50:09 -0300

    05/10/2023 15:50

--- api/src/controllers/getTypesPokemon.js
+++ api/src/controllers/getTypesPokemon.js
@@ -1,14 +1,29 @@
 const axios = require("axios")
 const url = 'https://pokeapi.co/api/v2/type'
+const {PokemonType} = require("../db.js")
 
 const getTypesPokemon = async(req,res) => {
     try {
         const {data} = await axios(url)
-        const pokemonTypes = data.results.map(e => {
+        const pokemonTypesNames = data.results.map(e => {
             return e.name
         })
 
-        res.status(200).json(pokemonTypes)
+        const pokemonTypesPromises = pokemonTypesNames.map(async e => {
+            const [pokemonType] = await PokemonType.findOrCreate({
+                where:{type:e},
+                defaults:{
+                    type:e
+                }
+            })
+            console.log(pokemonType)
+            return pokemonType.dataValues
+        })
+
+        Promise.all(pokemonTypesPromises)
+        .then(response => {
+            res.status(200).json(response)
+        })
     } catch (error) {
         res.status(500).send(error.message)
     }
--- api/src/controllers/postPokemon.js
+++ api/src/controllers/postPokemon.js
@@ -1,14 +1,14 @@
 const axios = require("axios")
 const { v4: uuidv4 } = require('uuid');
-const {Pokemon} = require("../db.js")
+const {Pokemon, PokemonType} = require("../db.js")
 
 const postPokemon = async(req,res) => {
     try {
-        const {name, image, health, attack, defense, speed, heigth, weigth} = req.body;
-        if(name === undefined || image === undefined || health === undefined || attack === undefined || defense === undefined){
+        const {name, image, health, attack, defense, types, speed, heigth, weigth} = req.body;
+        if(name === undefined || image === undefined || health === undefined || attack === undefined || defense === undefined || types === undefined){
             return res.status(400).send({error:"Faltan datos"})
         }
-        if(health < 0 || attack < 0 || defense < 0 || speed < 0 || heigth < 0 || weigth < 0){
+        if(types.length < 2 || health < 0 || attack < 0 || defense < 0 || speed < 0 || heigth < 0 || weigth < 0){
             return res.status(400).send({error:"Datos Incorrectos"})
         }
         const [pokemon, created] = await Pokemon.findOrCreate({
@@ -23,8 +23,11 @@ const postPokemon = async(req,res) => {
                 speed: null || speed,
                 heigth: null || heigth,
                 weigth: null || weigth,
-            }
+            },
+            include:PokemonType,
         })
+
+        pokemon.addPokemonType(types)
         if(created) return res.status(200).send(pokemon)
         res.status(200).send(`Ya hay un Pokemon creado con el nombre ${name}`)
 
