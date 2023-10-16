53ad160cbdd2d9ddfc104efdea7ae1faa9bcd8b7
Lautaro Bunkowski <lautibunko@gmail.com>
2023-10-16 19:44:47 -0300

    16/10/2023 19:44

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 require('dotenv').config();
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 
 // Syncing all the models at once.
 conn.sync({ force: true})
