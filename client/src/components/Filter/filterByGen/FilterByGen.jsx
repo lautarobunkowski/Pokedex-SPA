3543c4597137afa3662c71bd7b09a06eaeeeff6d
lautaro_Bunkowski <lautibunko@gmail.com>
2023-10-28 11:49:02 -0300

    28/10/2023 11:48

--- client/src/components/Filter/filterByGen/FilterByGen.jsx
+++ client/src/components/Filter/filterByGen/FilterByGen.jsx
@@ -5,22 +5,27 @@ import { useDispatch, useSelector } from "react-redux";
 
 const FilterByGen = () => {
   const firstGenGames = ["red", "yellow", "blue", "fireRed", "leafGreen"]
-  const [currentGen, setCurrentGen] = useState(0)
+  const [currentGen, setCurrentGen] = useState({
+    "firstGen": 0,
+    "secondGen": 0,
+    "thirdGen": 0,
+  })
 
-  const goToPreviousGen = () => {
-    setCurrentGen((prevGen) => (prevGen === 0 ? firstGenGames.length - 1 : prevGen - 1));
-  };
-  
-  const goToNextGen = () => {
-    setCurrentGen((prevGen) => (prevGen === firstGenGames.length - 1 ? 0 : prevGen + 1));
-  };
+  const handleClick = (event) => {
+    const nameTarget = event.target.name;
+    if(nameTarget === "prevButton"){
+      setCurrentGen((prevState) => (prevState.firstGen === 0 ? {...prevState, firstGen:firstGenGames.length - 1} : {...prevState, firstGen:prevState.firstGen - 1}));
+    } else {
+      setCurrentGen((prevState) => (prevState.firstGen === firstGenGames.length - 1 ? {...prevState, firstGen: 0} : {...prevState, firstGen:prevState.firstGen + 1}));
+    }
+  }
 
   return (
     <div className={styles.FilterByGen}>
       <div className={styles.firstGen}>
-        <button onClick={goToPreviousGen}>{`<`}</button>
-        <input className={styles.firstGenGames} name="firstGen" type="image" src={`./utils/generationsGames/first gen/${firstGenGames[currentGen]}.png`} alt={firstGenGames[currentGen]}/>
-        <button onClick={goToNextGen}>{`>`}</button>
+        <button name="prevButton" onClick={handleClick}>{`<`}</button>
+        <input className={styles.firstGenGames} name="firstGen" type="image" src={`./utils/generationsGames/first gen/${firstGenGames[currentGen.firstGen]}.png`} alt={firstGenGames[currentGen]}/>
+        <button name="nextButton" onClick={handleClick}>{`>`}</button>
       </div>
     </div>
   )
